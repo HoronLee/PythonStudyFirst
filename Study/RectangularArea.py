10bbff9cd1e9ed6bed6ecf52c6f88dfedf809817
HoronLee <1269024821@qq.com>
2022-10-10 17:12:14 +0800

    Changes

---
+++ Study/RectangularArea.py
@@ -0,0 +1,6 @@
+l = input("请输入矩形的长")    #输入长宽初始变量
+w = input("请输入矩形的宽")
+long = int(l)               #将初始变量转化为整型变量
+width = int(w)
+area = long * width         #乘法
+print("该矩形的面积是",area)   #输出
--- Study/hello.py
+++ Study/hello.py
@@ -1,3 +1,13 @@
+import random
+
 print("Hi,everyone")
 print("My first application")
 print("Is it simple?")
+print("富强民主文明和谐")
+print("自由平等公正法制")
+print("爱国敬业诚信友善")
+a = input("请输入一个整数")
+a = int(a)
+b = random.random()
+print("输入的数值：%d" %a)
+print("随机数值：%f" %b)
