4d65ee38819a0bf8752f474ec9e72a18457b044e
Horon_Lee <1269024821@qq.com>
2022-10-12 17:19:53 +0800

    格式化输出

---
+++ Study/OutputFormat.py
@@ -0,0 +1,7 @@
+name = "皓月互联"
+set_up_year = 2022
+lovely_num = 114.51
+print(f"我们是{name}团队,成立于{set_up_year},我们最喜欢的数字是{lovely_num}")
+print("=====================")
+print("1*1的结果是：%d"%(1*1))
+print("1*2的结果是：%d"%(1*2))
